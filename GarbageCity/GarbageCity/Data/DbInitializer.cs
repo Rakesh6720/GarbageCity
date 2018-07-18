1ed5b3ee3352c86e5599f0e103edbeb92b0677dc
Rakesh Baruah <rbaruah@gmail.com>
2018-07-18 10:54:18 -0500

    update w/ customer seed

--- GarbageCity/GarbageCity/Data/DbInitializer.cs
+++ GarbageCity/GarbageCity/Data/DbInitializer.cs
@@ -11,34 +11,36 @@ namespace GarbageCity.Data
         public static void Initialize(CompanyContext context)
         {
             //context.Database.EnsureCreated();
-            if (context.Customer.Any())
+            if (!context.Customer.Any())
             {
-                return;
-            }
-
-            var customers = new Customer[]
+                var customers = new Customer[]
             {
                 new Customer{LastName="Alexander", FirstMidName="Carson", Address="44 Bilbo Road", ZipCode=11111, PickupDay="Monday"},
                 new Customer{FirstMidName="Meredith", LastName="Alonso", Address="55 Pineapple Way", ZipCode=22222, PickupDay="Tuesday"},
                 new Customer{FirstMidName="Arturo", LastName="Anand", Address="66 Lucy Lane", ZipCode=33333, PickupDay="Wednesday"}
             };
-            foreach (Customer c in customers)
-            {
-                context.Customer.Add(c);
+                foreach (Customer c in customers)
+                {
+                    context.Customer.Add(c);
+                }
+                context.SaveChanges();
             }
-            context.SaveChanges();
 
-            var employees = new Employee[]
+            if (!context.Employee.Any())
             {
+                var employees = new Employee[]
+                {
                 new Employee{FirstMidName="Gavin", LastName="Rossdale", EmployeeID=11111 },
                 new Employee{FirstMidName="Gwen", LastName="Stefani", EmployeeID=22222},
                 new Employee{FirstMidName="Avery", LastName="Carmichael", EmployeeID=33333}
-            };
-            foreach (Employee e in employees)
-            {
-                context.Employee.Add(e);
+                };
+                foreach (Employee e in employees)
+                {
+                    context.Employee.Add(e);
+                }
+                context.SaveChanges();
+                return;
             }
-            context.SaveChanges();
         }
     }
 }
